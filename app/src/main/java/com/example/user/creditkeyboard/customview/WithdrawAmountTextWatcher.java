af98d613ce27ca1329c8602d1ec09b3063852c8b
Jsion <guofeilongmr@qq.com>
2016-12-07 15:18:46 +0800

    加入删除时候的逻辑判断
    加入删除时候的逻辑判断,如果就剩下一位小数连点一起删除,

--- app/src/main/java/com/example/user/creditkeyboard/customview/WithdrawAmountTextWatcher.java
+++ app/src/main/java/com/example/user/creditkeyboard/customview/WithdrawAmountTextWatcher.java
@@ -39,7 +39,7 @@ public class WithdrawAmountTextWatcher implements TextWatcher {
         changeTC = s.toString();
         Log.e(TAG, "onTextChanged: --->changeTC--->" + changeTC + "---count--->" + count);
 
-
+        // 第一位不允许输入字符 "."
         if (changeTC.length() >= 1) {
             currentInputDesc = changeTC.charAt(changeTC.length() - 1) + "";
             if (TextUtils.isEmpty(beforTC)) {
@@ -47,14 +47,14 @@ public class WithdrawAmountTextWatcher implements TextWatcher {
                     editText.setText("");
                 }
             }
-
+            // 如果第一位是0,那么第二位只能输入字符 ".",不允许出现0000.232的情况
             if (beforTC.equals("0")) {
                 if (!currentInputDesc.equals(".")) {
                     editText.setText(beforTC);
                     editText.setSelection(beforTC.length() - 1);
                 }
             }
-
+            // 如果输入的字符串中包含一个. 那么根据保留小数的个数进行重新组装
             if (!TextUtils.isEmpty(changeTC)) {
                 String lastChar = String.valueOf(changeTC.charAt(changeTC.length() - 1));
                 String firstChar = String.valueOf(changeTC.charAt(0));
@@ -68,6 +68,20 @@ public class WithdrawAmountTextWatcher implements TextWatcher {
                         editText.setText(s1 + "." + s3);
                         editText.setSelection(changeTC.length());
                     }
+                }
+            }
+        }
+
+        // 删除字符的情况,如果当前内容的最后两位是"点+数字"那么删除的时候连点一起删除
+        if (count == 0) {
+            if (!TextUtils.isEmpty(changeTC)) {
+                String lastChar = String.valueOf(changeTC.charAt(changeTC.length() - 1));
+                String firstChar = String.valueOf(changeTC.charAt(0));
+                if (changeTC.contains(".") && lastChar.equals(".") && !firstChar.equals(".")) {
+                    String[] split = changeTC.split("\\.");
+                    String s1 = split[0];
+                    editText.setText(s1);
+                    editText.setSelection(editText.getText().length());
                 }
             }
         }
